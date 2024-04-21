5e08aa8d9e57370f0ea71ac40176b352cfe15440
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-22 03:35:08 +0400

    New Branch Models: Create the base model for the common attr. and methods

---
+++ server/models/test_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+
+from basemodel import Base, BaseModel
+
+engine = create_engine("sqlite:///:memory:")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class TestModel(Base, BaseModel):
+    __tablename__ = "test_model"
+    test_field = Column(String(50))
+
+
+Base.metadata.create_all(engine)
+
+test_instance = TestModel(test_field="Hello, world!")
+session.add(test_instance)
+session.commit()
+
+queried_instance = session.query(TestModel).first()
+print(queried_instance.id)
+print(queried_instance.created_at)
+print(queried_instance.updated_at)
+print(queried_instance.test_field)
+print(queried_instance.to_dict)
