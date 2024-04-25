2aec9fc055f8298b024e01f6b5e91c4899c6e1cd
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-26 00:41:12 +0400

    Fix HTTP response, unified api response into JSON format

---
+++ server/api/app.py
@@ -0,0 +1,114 @@
+"""
+This module initializes and configures the Flask application.
+
+The application is configured with CORS, session management,
+and user authentication.
+It also registers the user-related routes from the `user_views` blueprint.
+
+Modules:
+    Flask: The main class for all Flask applications.
+    Bcrypt: Used for hashing passwords.
+    CORS: A Flask extension for handling Cross Origin Resource Sharing (CORS),
+    making cross-origin AJAX possible.
+    LoginManager: Provides user session management for Flask.
+    Session: Provides server-backed sessions for Flask.
+
+Imports:
+    server.api.views.user_views: Contains the user-related routes.
+    server.config: Contains the application configuration.
+    server.models: Contains the SQLAlchemy models and storage helper.
+
+
+"""
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_login import LoginManager
+from flask_session import Session
+from server.api.utils import make_response
+from server.api.views.user_views import set_bcrypt
+from server.config import ApplicationConfig
+from server.models import storage
+from server.models.user import User
+
+app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
+app.url_map.strict_slashes = False
+
+bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+server_session = Session(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    Load a user given the user_id.
+
+    Args:
+        user_id (str): The unique identifier of the user.
+
+    Returns:
+        User: The User object if found, None otherwise.
+    """
+    return storage.get(User, user_id)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirects unauthorized users to a custom HTML page."""
+    return make_response("error", "Unauthorized"), 401
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return make_response("error", "Bad Request"), 400
+
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return make_response("error", "Unauthorized"), 401
+
+
+@app.errorhandler(403)
+def forbidden(e):
+    return make_response("error", "Forbidden"), 403
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return make_response("error", "Not Found"), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return make_response("error", "Method Not Allowed"), 405
+
+
+@app.errorhandler(408)
+def request_timeout(e):
+    return make_response("error", "Request Timeout"), 408
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return make_response("error", "Internal Server Error"), 500
+
+
+set_bcrypt(bcrypt)
+
+# Import Blueprints
+
+from server.api.views.candidate_views import candidate_views
+from server.api.views.user_views import user_views
+
+# Register Blueprints
+app.register_blueprint(user_views, url_prefix="/api")
+app.register_blueprint(candidate_views, url_prefix="/api")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ server/api/views/user_views.py
@@ -0,0 +1,189 @@
+"""
+This module defines the user-related routes for the Flask application.
+
+The routes include user registration, login, logout, and fetching the
+current user's details.
+Each route validates the incoming data using the appropriate Marshmallow
+schema and performs the necessary operations.
+
+Modules:
+    flask: The main class for all Flask applications.
+    flask_login: Provides user session management for Flask.
+    marshmallow: Used for data validation.
+    server.models: Contains the SQLAlchemy models and storage helper.
+    server.models.user: The User SQLAlchemy model.
+    .schemas: Contains the data validation schemas for user login and
+    registration.
+
+Functions:
+    set_bcrypt(bcrypt_instance): Sets bcrypt instance for password hashing.
+    make_response(status, message, data): Creates a unified response format.
+"""
+from flask import Blueprint, request, session
+from flask_login import login_required, login_user, logout_user
+from marshmallow import ValidationError
+from server.api.utils import make_response
+from server.models import storage
+from server.models.user import User
+
+from .schemas import login_schema, registration_schema, update_schema
+
+user_views = Blueprint("user", __name__)
+
+
+def set_bcrypt(bcrypt_instance):
+    """
+    Sets the bcrypt instance for password hashing.
+
+    Args:
+        bcrypt_instance: The bcrypt instance.
+    """
+    global bcrypt
+    bcrypt = bcrypt_instance
+
+
+@user_views.route("/register", methods=["POST"])
+def register_user():
+    """
+    Handles user registration.
+
+    Validates the incoming data and creates a new user if the validation
+    is successful.
+    """
+    try:
+        data = registration_schema.load(request.json)
+    except ValidationError as err:
+        return make_response("error", err.messages), 400
+
+    user = storage.get_user_by_email(data["email"])
+    if user:
+        return make_response("error", "User already exists"), 409
+
+    hashed_password = bcrypt.generate_password_hash(data["password"])
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        password=hashed_password,
+        role=data["role"],
+    )
+    storage.new(new_user)
+    storage.save()
+
+    session["user_id"] = new_user.id
+    session["role"] = new_user.role
+
+    return (
+        make_response(
+            "success",
+            "User registered successfully",
+            {"id": new_user.id, "role": new_user.role},
+        ),
+        201,
+    )
+
+
+@user_views.route("/login", methods=["POST"])
+def login_user_():
+    """
+    Handles user login.
+
+    Validates the incoming data and logs in the user if the validation is
+    successful.
+    """
+    try:
+        data = login_schema.load(request.json)
+    except ValidationError as err:
+        return make_response("error", err.messages), 400
+
+    user = storage.get_user_by_email(data["email"])
+    if not user or not bcrypt.check_password_hash(user.password, data["password"]):
+        return make_response("error", "Unauthorized"), 401
+
+    login_user(user)
+    session["user_id"] = user.id
+    session["role"] = user.role
+
+    return make_response(
+        "success", "User logged in successfully", {"id": user.id, "role": user.role}
+    )
+
+
+@user_views.route("/logout", methods=["POST"])
+@login_required
+def logout_user_():
+    """
+    Handles user logout.
+
+    Logs out the user if they are currently logged in.
+    """
+    if "user_id" in session:
+        session.pop("user_id")
+        session.pop("role")
+        logout_user()
+        return make_response("success", "Logged out successfully")
+    else:
+        return make_response("error", "No active session"), 400
+
+
+@user_views.route("/@me")
+@login_required
+def get_current_user():
+    """
+    Fetches the current user's details.
+
+    Returns the details of the user who is currently logged in.
+    """
+    user_id = session.get("user_id")
+    if not user_id:
+        return make_response("error", "Unauthorized"), 401
+
+    user = storage.get(User, user_id)
+    return make_response(
+        "success",
+        "User details fetched successfully",
+        {"id": user.id, "role": user.role},
+    )
+
+
+@user_views.route("/@me", methods=["PUT"])
+@login_required
+def update_current_user():
+    """Update the current user's details."""
+    try:
+        data = update_schema.load(request.json)
+    except ValidationError as err:
+        return make_response("error", err.messages), 400
+
+    user_id = session.get("user_id")
+    user = storage.get(User, user_id)
+    if not user:
+        return make_response("error", "Unauthorized"), 401
+
+    for key, value in data.items():
+        setattr(user, key, value)
+    storage.save()
+
+    return make_response(
+        "success",
+        "User details updated successfully",
+        {"id": user.id, "role": user.role},
+    )
+
+
+@user_views.route("/@me", methods=["DELETE"])
+@login_required
+def delete_current_user():
+    """Delete the current user."""
+    user_id = session.get("user_id")
+    user = storage.get(User, user_id)
+    if not user:
+        return make_response("error", "Unauthorized"), 401
+
+    storage.delete(user)
+    storage.save()
+
+    session.pop("user_id")
+    session.pop("role")
+    logout_user()
+
+    return make_response("success", "User deleted successfully")
