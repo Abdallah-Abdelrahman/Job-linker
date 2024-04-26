49502b1880afcde59966d90046a3f57b4dbc856c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-27 02:42:44 +0300

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import Login from "./Login";
 import Register from "./Register";
 import "./App.css";
+import { Link, Outlet, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Job Linker</h1>
-      <Login />
-      <Register />
     </div>
   );
 };
+function Layout() {
+  return (
+    <div style={{ width: '100%', height: '100%', padding: '1rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <header style={{ width: '100%' }}>
+        <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '1.5rem' }}>
+          <li><Link to='/'> home </Link></li>
+          <li><Link to='login'>login</Link></li>
+          <li><Link to='signup'>signup</Link></li>
+        </ul>
+      </header>
+      <main style={{margin: 'auto'}}><Outlet /></main>
+      <footer style={{ marginTop: 'auto' }}>ATS &copy; mohanad & abdallah</footer>
 
+    </div>
+
+  )
+}
+export const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
+    <Route index element={<App />} />
+    <Route path='login' element={<Login />} />
+    <Route path='signup' element={<Register />} />
+  </Route>
+))
 export default App;
