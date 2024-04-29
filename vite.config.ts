9d215d8a16d2038b8b19545ff61dc28efbae215b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-29 06:16:19 +0300

    Complete mail service

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const env = {
+  development: 'http://0.0.0.0:5000',
+  test: 'http://0.0.0.0:5000',
+  production: ''
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://0.0.0.0:5000",
+      '/api': {
+        target: env[process.env.NODE_ENV],
         changeOrigin: true,
       },
     },
