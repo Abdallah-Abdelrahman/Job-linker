bd846f6068dfbc434b004423d5cc6f6f6ca2a4da
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-30 04:14:47 +0400

    Email verfication

--- server/api/views/user_views.py
+++ server/api/views/user_views.py
@@ -1,12 +1,16 @@
 """
 This module provides views for the User model in the Job-linker application.
 """
-
 from flask import Blueprint, request
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    set_refresh_cookies,
+)
 
 from server.api.utils import make_response_
-from server.controllers.user_controller import UserController
+from server.decorators import verified_required
 
 user_views = Blueprint("user", __name__)
 
@@ -14,16 +18,15 @@ bcrypt = None
 user_controller = None
 
 
-def set_bcrypt(bcrypt_instance):
+def setup(controller):
     """
     Sets the bcrypt instance for password hashing.
 
     Args:
         bcrypt_instance: The bcrypt instance.
     """
-    global bcrypt, user_controller
-    bcrypt = bcrypt_instance
-    user_controller = UserController(bcrypt)
+    global user_controller
+    user_controller = controller
 
 
 @user_views.route("/register", methods=["POST"])
@@ -37,15 +40,32 @@ def register_user():
         Otherwise, it returns an error message.
     """
     try:
-        new_user, access_token = user_controller.register_user(request.json)
-        return (
-            make_response_(
-                "success",
-                "User registered successfully",
-                {"access_token": access_token, "role": new_user.role},
-            ),
-            201,
+        new_user = user_controller.register_user(request.json)
+        response = make_response_(
+            "success",
+            "User registered successfully",
+            {"role": new_user.role},
         )
+    except ValueError as e:
+        return make_response_("error", str(e)), 400
+
+    return response, 201
+
+
+@user_views.route("/verify/<token>", methods=["GET"])
+def verify_email(token):
+    """
+    Verifies a user's email.
+
+    Args:
+        token (str): The verification token.
+
+    Returns:
+        A response object containing the status and message.
+    """
+    try:
+        user_controller.verify_email(token)
+        return make_response_("success", "Your email has been verified!"), 200
     except ValueError as e:
         return make_response_("error", str(e)), 400
 
@@ -61,8 +81,10 @@ def login_user():
         Otherwise, it returns an error message.
     """
     try:
-        user, access_token = user_controller.login_user(request.json)
-        return make_response_(
+        user, access_token, refresh_token = user_controller.login_user(
+                request.json
+                )
+        response_data = make_response_(
             "success",
             "User logged in successfully",
             {"access_token": access_token, "role": user.role},
@@ -70,9 +92,29 @@ def login_user():
     except ValueError as e:
         return make_response_("error", str(e)), 401
 
+    response = response_data
+    set_refresh_cookies(response, refresh_token)
+    return response
+
+
+@user_views.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh_token():
+    user_id = get_jwt_identity()
+    access_token = create_access_token(identity=user_id)
+    return (
+        make_response_(
+            "success",
+            "Token refreshed successfully",
+            {"access_token": access_token},
+        ),
+        200,
+    )
+
 
 @user_views.route("/logout", methods=["POST"])
 @jwt_required()
+@verified_required
 def logout_user():
     """
     Logs out a user.
@@ -85,6 +127,7 @@ def logout_user():
 
 @user_views.route("/@me")
 @jwt_required()
+@verified_required
 def get_current_user():
     """
     Fetches the current user's details.
@@ -108,6 +151,7 @@ def get_current_user():
 
 @user_views.route("/@me", methods=["PUT"])
 @jwt_required()
+@verified_required
 def update_current_user():
     """
     Updates the current user's details.
@@ -131,6 +175,7 @@ def update_current_user():
 
 @user_views.route("/@me", methods=["DELETE"])
 @jwt_required()
+@verified_required
 def delete_current_user():
     """
     Deletes the current user.
