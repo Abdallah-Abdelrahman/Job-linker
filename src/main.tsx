5c2c26af36559f0560069abb5cf51cfb46b781c2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-16 22:04:36 +0300

    Add single job route for recruiter's jobs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useParams } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -13,6 +13,11 @@ import { ErrorPage, Layout, Private } from './components';
 import { Verify, Register, Login, Profile, Home } from './routes';
 import './index.css';
 
+function Job() {
+  const param = useParams();
+
+  return (<h1>{param.job_id}</h1>);
+}
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} errorElement={<ErrorPage />}>
@@ -21,7 +26,9 @@ const router = createBrowserRouter(
       <Route path='login' element={<Login />} />
       <Route path='verify' element={<Verify />} />
       <Route element={<Private />}>
-        <Route path='@me' element={<Profile />} />
+        <Route path='@me' element={<Profile />}>
+          <Route path='jobs/:job_id' element={<Job />} />
+        </Route>
       </Route>
     </Route>
   ),
