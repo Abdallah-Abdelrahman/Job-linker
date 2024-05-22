26ad25ec3852cdcea5e908c837eaf86e2ece358f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-22 19:26:59 +0300

    Add `HOST` variable environment

--- src/routes/Verify.tsx
+++ src/routes/Verify.tsx
@@ -3,31 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import { useLazyVerfiyQuery } from '../app/services/auth';
 import { useAppDispatch } from '../hooks/store';
 import { setCredentials } from '../features/auth';
-import { SkeletonText } from '@chakra-ui/react';
+import { Skeleton, SkeletonText } from '@chakra-ui/react';
 
 function Verify() {
   // extarct the query from url
-  const parmas = new URLSearchParams(window.location.search);
   const navigate = useNavigate();
-  const token = parmas.get('token');
   const [verify, { isLoading, isUninitialized }] = useLazyVerfiyQuery();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
+    const parmas = new URLSearchParams(window.location.search);
+    const token = parmas.get('token')!;
+    console.log('-----token------>', { token });
+    if (isUninitialized) {
+      dispatch(setCredentials({ isRefreshing: true }));
+    }
     verify({ token })
       .unwrap()
       .then(data => {
-        console.log('------verify--------->', {data});
-        setCredentials(data.data);
+        console.log('------verify--------->', { data });
+        dispatch(setCredentials(
+          {
+            ...data.data,
+            isRefreshed: true,
+            isRefreshing: false
+          }));
         navigate('/@me');
       })
-      .catch(_ => navigate('/login'));
-  }, [verify, token, dispatch, navigate]);
+      .catch(_ => navigate('/login'))
+      .finally(() => {
+        dispatch(setCredentials({ isRefreshing: false }));
+      });
+  }, [verify, dispatch, navigate, isUninitialized]);
 
-  if (isUninitialized || isLoading) {
-    return (<SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />);
-  }
-  return (null);
+  return (
+    <Skeleton size='lg' isLoaded={isUninitialized || !isLoading} />
+  );
 }
 
 export default Verify;
