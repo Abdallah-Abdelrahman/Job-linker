02a28d2d3f05acfd60904d81530400679f31e987
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-23 15:31:25 +0300

    Add skill update on user profile

--- src/app/services/skill.ts
+++ src/app/services/skill.ts
@@ -1,4 +1,4 @@
-import { api } from "./auth";
+import { api } from './auth';
 
 export interface Skill {
   skill_id: string;
@@ -12,29 +12,31 @@ export interface SkillResponse {
 export const skillApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getSkills: builder.query<Skill[], void>({
-      query: () => "skills",
+      query: () => 'skills',
     }),
     createSkill: builder.mutation<SkillResponse, Partial<Skill>>({
       query: (skill) => ({
-        url: "skills",
-        method: "POST",
+        url: 'skills',
+        method: 'POST',
         body: skill,
       }),
     }),
     updateSkill: builder.mutation<
       SkillResponse,
-      { skill_id: string; updates: Partial<Skill> }
+      { skill_id: string; skill: Partial<Skill> }
     >({
-      query: ({ skill_id, updates }) => ({
-        url: `skills/${skill_id}`,
-        method: "PUT",
-        body: updates,
-      }),
+      query: ({ skill_id, skill }) => {
+        return ({
+          url: `skills/${skill_id}`,
+          method: 'PUT',
+          body: skill,
+        });
+      },
     }),
     deleteSkill: builder.mutation<void, { skill_id: string }>({
       query: ({ skill_id }) => ({
         url: `skills/${skill_id}`,
-        method: "DELETE",
+        method: 'DELETE',
       }),
     }),
   }),
--- src/components/profile/Candidate.tsx
+++ src/components/profile/Candidate.tsx
@@ -1,27 +1,95 @@
-import { Text, Heading, Box, Stack } from '@chakra-ui/react';
+import { Text, Heading, Box, Stack, Button, ButtonGroup, Input, InputGroup, InputLeftElement, Textarea, FormControl } from '@chakra-ui/react';
 import MyIcon from '../Icon';
 import * as T from './types';
+import { Reducer, useReducer, useState } from 'react';
+import { useUpdateSkillMutation } from '../../app/services/skill';
+
+type ActionType = 'contact_info' | undefined
+type S = Omit<T.CandidateProp['data'], 'candidate'> & { major: string }
+type A = {
+  type?: ActionType,
+  payload: Partial<S>
+}
+/**
+ * custom function to create action
+ */
+const actionCreator = (payload: A['payload'], type?: A['type']) => ({ payload, type });
 
 
 function Candidate({ data }: T.CandidateProp) {
+  const [isEditingInfo, setIsEditiing] = useState(false);
+  const [state, dispatch] = useReducer<Reducer<S, A>>(
+    (state, action) => {
+      const { type, payload } = action;
+      if (!type) {
+        return { ...state, ...payload };
+      }
+      return ({
+        ...state,
+        contact_info: { ...state.contact_info, ...payload }
+      });
+    },
+    {
+      name: data.name ?? '',
+      email: data.email ?? '',
+      image_url: data.image_url ?? '',
+      contact_info: data.contact_info ?? '',
+      bio: data.bio ?? '',
+      major: data.candidate.major.name ?? ''
+    }
+  );
+
   return (
     <Box className='grid grid-cols-4 gap-6 container mt-4 mx-auto sm:grid-cols-12'>
-      <Box className='col-span-4 bg-white flex p-6 flex-col items-center gap-2 rounded-md shadow-md sm:col-span-4'>
-        <Box className='w-32 h-32  rounded-full overflow-hidden'>
-          <img src='https://placehold.co/600x400' className='w-full h-full object-cover' />
+      {/* Contact & skills */}
+      <Box
+        className='relative  col-span-4 bg-white flex p-6 flex-col items-center gap-2 rounded-md shadow-md sm:col-span-4 sm:max-h-[900px] sm:overflow-auto'
+      >
+        <Button
+          className='!absolute top-6 right-6'
+          onClick={() => setIsEditiing(true)}
+        >
+          <MyIcon href='/sprite.svg#edit' className='w-6 h-6' />
+        </Button>
+        <Box className='w-32 h-32 min-h-32 bg-red-200 rounded-full overflow-hidden'>
+          <img
+            src='https://placehold.co/600x400'
+            className='w-full h-full object-cover'
+          />
         </Box>
-        <Heading as='h2' size='lg' className='capitalize'>{data.name}</Heading>
-        <Text className='tracking-wide'>{data.candidate.major.name}</Text>
-        <hr className='w-full' />
+        {isEditingInfo
+          ?
+          <FormControl size='lg'>
+            <Input
+              value={state.name}
+              onChange={(e) => dispatch(actionCreator({ name: e.target.value }))}
+            />
+          </FormControl>
+          : <Heading as='h2' size='lg' className='capitalize'>{data.name}</Heading>}
 
+        {isEditingInfo
+          ? <FormControl size='lg'>
+            <Input
+              value={state.major}
+              onChange={(e) => dispatch(actionCreator({ major: e.target.value }))}
+            />
+          </FormControl>
+          : <Text className='tracking-wide'>{data.candidate.major.name}</Text>}
+
+        <hr className='w-full' />
         <Stack className='w-full mt-2 space-y-6'>
-          <Contact_info data={data.contact_info} />
+          <Contact_info
+            isEditingInfo={isEditingInfo}
+            dispatch={dispatch}
+            data={data.contact_info}
+            state={state}
+          />
           <Box as='section' className='flex flex-col gap-4'>
             {/*Skills*/}
             <Heading as='h4' mb='2' size='lg' className='capitalize'>skills</Heading>
             <ul className='flex flex-wrap gap-4 max-h-64 overflow-auto'>
               {data.candidate.skills.map((skill, idx) =>
-                <li key={idx} className='p-2 bg-teal-50 text-teal-500 rounded-tl-lg rounded-br-lg'>{skill.name}</li>
+                <Skill key={idx} skill={skill} />
               )}
             </ul>
           </Box>
@@ -40,17 +108,39 @@ function Candidate({ data }: T.CandidateProp) {
             </Box>
           </Box>
         </Stack>
+        {isEditingInfo &&
+          <ButtonGroup className='ml-auto'>
+            <Button
+              children='update'
+            />
+            <Button
+              onClick={() => setIsEditiing(false)}
+              children='cancel'
+            />
+          </ButtonGroup>
+        }
+
       </Box>
 
-      <Box className='col-span-4 bg-white flex flex-col rounded-md shadow-md p-6 gap-4 sm:col-span-8'>
+      <Box className=' col-span-4 bg-white flex flex-col rounded-md shadow-md p-6 gap-4 sm:col-span-8 sm:max-h-[900px] sm:overflow-auto'>
         {/*About*/}
         <Box>
           <Heading as='h4' mb='2' size='lg' className='capitalize'>
             About me
           </Heading>
-          <Text>
-            {data.bio}
-          </Text>
+          {isEditingInfo
+            ? <FormControl
+              size='lg'
+              children={
+                <Textarea
+                  value={state.bio}
+                  onChange={(e) => dispatch(actionCreator({ bio: e.target.value }))}
+                />}
+            />
+            : <Text>
+              {data.bio}
+            </Text>}
+
         </Box>
         {/*Experience*/}
         <Box>
@@ -140,7 +230,61 @@ function Candidate({ data }: T.CandidateProp) {
 
 }
 
-export function Contact_info({ data }: { data: Contact }) {
+type SkillProps = {
+  skill: T.Skill
+}
+
+function Skill({ skill }: SkillProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(skill.name);
+  const [update, { isLoading }] = useUpdateSkillMutation();
+  const handleUpdate = () => {
+    update({ skill_id: skill.id, skill: { name: value } })
+      .unwrap()
+      .then(_ => setIsEditing(false))
+      .catch(err => console.log({ err }))
+  }
+
+  return (
+    <li className='relative p-2 pr-4 bg-teal-50 text-teal-500 rounded-tl-lg rounded-br-lg'>
+
+      {isEditing
+        ? <>
+          <Input className='!w-max' value={value} onChange={(e) => setValue(e.target.value)} />
+          <ButtonGroup>
+            <Button
+              size='sm'
+              children='udpate'
+              isLoading={isLoading}
+              onClick={handleUpdate}
+            />
+            <Button
+              size='sm'
+              children='cancel'
+              onClick={() => setIsEditing(false)}
+            />
+          </ButtonGroup>
+        </>
+        : <>
+          <MyIcon
+            href='/sprite.svg#edit' className='absolute top-0 right-0 w-3 h-3 cursor-pointer'
+            onClick={() => setIsEditing(true)}
+          />
+          <Text>{value}</Text>
+        </>
+      }
+
+    </li>
+
+  );
+}
+type ContactProps = {
+  data: Record<'address' | 'github' | 'linkedin' | 'whatsapp' | 'phone', string>,
+  dispatch: React.Dispatch<A>,
+  isEditingInfo: boolean,
+  state: S
+}
+export function Contact_info({ data, isEditingInfo, state, dispatch }: ContactProps) {
   if (!data) {
     return (null);
   }
@@ -150,12 +294,34 @@ export function Contact_info({ data }: { data: Contact }) {
       <Heading as='h4' mb='2' size='lg' className='capitalize'>contact info</Heading>
       {Object.entries(data).map(([k, v]) => {
         if (!v) return (null);
-        return (<Box key={k} className='flex gap-3'>
-          <MyIcon href={`/sprite.svg#${k}`} className='w-5 h-5' />
-          {k == 'linkedin' || k == 'github'
-            ? <a href={v} target='_blank' className='text-sky-500'>{k}</a>
-            : <Text>{v}</Text>}
-        </Box>
+        return (
+          <Box key={k} className='flex gap-3'>
+            {isEditingInfo
+              ? (
+                <>
+                  <InputGroup>
+                    <InputLeftElement
+                      children={<MyIcon href={`/sprite.svg#${k}`} className='w-5 h-5' />}
+                    />
+                    <Input
+                      value={state.contact_info[k]}
+                      onChange={(e) => dispatch(actionCreator({ [k]: e.target.value }, 'contact_info'))}
+                    />
+                  </InputGroup>
+                </>
+              )
+              : (
+                <>
+                  <MyIcon href={`/sprite.svg#${k}`} className='w-5 h-5' />
+                  {k == 'linkedin' || k == 'github'
+                    ? <a href={v} target='_blank' className='text-sky-500'>{k}</a>
+                    : <Text>{v}</Text>
+                  }
+                </>
+              )
+            }
+
+          </Box>
         );
       }
       )}
